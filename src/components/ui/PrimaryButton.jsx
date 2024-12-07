a7d9b0df96499079576a2cf2f11ecd50b7b7dbe9
juju-anselum <anselumjuju@gmail.com>
2024-12-07 20:38:28 +0530

    Update Button.jsx

--- src/components/ui/PrimaryButton.jsx
+++ src/components/ui/PrimaryButton.jsx
@@ -1,14 +1,21 @@
-import { ArrowRight } from "lucide-react"
+import { cn } from "../../utils/utils";
 
-const PrimaryButton = ({ text, isIcon, onClick }) => {
+const PrimaryButton = ({ text, onClick, startIcon: StartIcon, endIcon: EndIcon, isResponsive, variant, className }) => {
+	const baseStyles = 'w-max px-6 py-3 rounded-full flex gap-x-3 group'
+	const variantStyles = variant == 'outlined'
+		? 'bg-transparent border border-gray text-gray'
+		: `${isResponsive && 'bg-transparent border border-gray text-gray'} md:bg-gray md:text-white`;
+
 	return (
 		<button
-			className="w-max px-6 py-3 rounded-full bg-gray text-white flex gap-x-3 group"
+			className={cn(`${baseStyles} ${variantStyles}`, className)}
+			onClick={onClick}
 		>
-			{text}
-			{isIcon && <ArrowRight className="w-5 group-hover:w-6 transition-all duration-150" />}
+			{StartIcon && <StartIcon className="w-5 group-hover:w-6 transition-all duration-150" />}
+			<span className={`${isResponsive && 'hidden md:block'}`}>{text}</span>
+			{EndIcon && <EndIcon className="w-5 group-hover:w-6 transition-all duration-150" />}
 		</button>
-	)
-}
+	);
+};
 
-export default PrimaryButton
+export default PrimaryButton;
